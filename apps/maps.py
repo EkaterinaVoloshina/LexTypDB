f4e66ab98e4b7c6389ab0531eda1ed0a6056b318
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-04-26 23:19:40 +0300

    Update maps.py

--- apps/maps.py
+++ apps/maps.py
@@ -1,10 +1,11 @@
 #import lingtypology
 import streamlit as st
 import streamlit.components.v1 as components
+from .database_utils import get_db
 
 def app():
-    pass
-#    languages = db.languages.find().distinct('lang')
-#    m = lingtypology.LingMap(languages)
-#    m.save('map.html')
-#    components.html(open('map.html', 'r', encoding='utf-8').read(), height=1500, width=1500)
+    db = get_db()
+    languages = db.languages.find().distinct('lang')
+    m = lingtypology.LingMap(languages)
+    m.save('map.html')
+    components.html(open('map.html', 'r', encoding='utf-8').read(), height=1500, width=1500)
