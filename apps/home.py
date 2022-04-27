77539a7dc3115e7ed270ec04db51bdf1a0d74f1a
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-04-28 00:08:55 +0300

    updated home page

--- apps/home.py
+++ apps/home.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 def app():
     st.title("База данных по лексической типологии")
     st.markdown("""По всем вопросам обращайтесь по адресу: <a href="mailto:vokat@mail.ru" subject="LexTyp Database">vokat@mail.ru</a>""")
