585b10404a18a4586d4a55ac822c82b1d018c969
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-04-28 00:12:06 +0300

    fixed html

--- apps/home.py
+++ apps/home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def app():
     st.title("База данных по лексической типологии")
-    st.markdown("""По всем вопросам обращайтесь по адресу: <a href="mailto:vokat@mail.ru" subject="LexTyp Database">vokat@mail.ru</a>""")
+    st.markdown("""По всем вопросам обращайтесь по адресу: <a href="mailto:vokat@mail.ru" subject="LexTyp Database">vokat@mail.ru</a>""", unsafe_allow_html=True)
