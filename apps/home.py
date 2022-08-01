56a792d3905661b30ef151ebdbb01feede1284fe
Ekaterina Voloshina <vokat@mail.ru>
2022-08-01 11:19:37 +0300

    changed description

--- apps/home.py
+++ apps/home.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 def app():
     st.title("База данных по лексической типологии")
+    st.markdown("*Здесь будет описание проекта*")
     st.markdown("""По всем вопросам обращайтесь по адресу: <a href="mailto:vokat@mail.ru" subject="LexTyp Database">vokat@mail.ru</a>""", unsafe_allow_html=True)
